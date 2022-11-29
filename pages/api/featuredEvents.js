afa20aa8769d39be622be7ecaf59b5f51c2af23e
Johannes Weniger <jo@MacBook-Pro-2.Home>
2022-11-29 18:05:57 +0000

    commented out og image

--- pages/api/featuredEvents.js
+++ pages/api/featuredEvents.js
@@ -1,14 +1,14 @@
-import { getAllEvents } from "../../services/sort-event-data";
+// import { getAllEvents } from "../../services/sort-event-data";
 
-export default async function handler(req, res) {
+// export default async function handler(req, res) {
     
-    if (req.method !== 'GET') {
-      res.status(405).send({ message: 'Only GET requests allowed' })
-      return
-    }
-    const {featuredEvents} = await getAllEvents();
-    res.status(200).json({featuredEvents});
-    return
+//     if (req.method !== 'GET') {
+//       res.status(405).send({ message: 'Only GET requests allowed' })
+//       return
+//     }
+//     const {featuredEvents} = await getAllEvents();
+//     res.status(200).json({featuredEvents});
+//     return
     
   
-  }
+//   }
--- pages/api/og-image.tsx
+++ pages/api/og-image.tsx
@@ -1,68 +1,68 @@
-import { ImageResponse } from "@vercel/og";
-import { NextRequest } from "next/server";
-import React from "react";
-import { getAllEvents } from "../../services/sort-event-data";
+// import { ImageResponse } from "@vercel/og";
+// import { NextRequest } from "next/server";
+// import React from "react";
+// import { getAllEvents } from "../../services/sort-event-data";
 
-export const config = {
-  runtime: "experimental-edge",
-};
+// export const config = {
+//   runtime: "experimental-edge",
+// };
 
-export const experiments = {
-  topLevelAwait: true,
-}
+// export const experiments = {
+//   topLevelAwait: true,
+// }
 
-const websiteUrl = "http://localhost:3000";
+// const websiteUrl = "http://localhost:3000";
 
-const font = fetch(new URL(`${websiteUrl}/fonts/ABC_Monument_Grotesk/ABCMonumentGrotesk-Medium.otf`, import.meta.url)).then(
-  (res) => res.arrayBuffer(),
-);
-
-
+// const font = fetch(new URL(`${websiteUrl}/fonts/ABC_Monument_Grotesk/ABCMonumentGrotesk-Medium.otf`, import.meta.url)).then(
+//   (res) => res.arrayBuffer(),
+// );
 
 
 
 
-async function handler() {
-  const fontData = await font;
 
-  const { upcomingEventsAsc, pastEventsDesc, featuredEvents } = await getAllEvents();
-  console.log(upcomingEventsAsc)
 
-  return new ImageResponse(
-    (
-      <div tw="flex h-full ">
-        <div tw="flex flex-col w-1/2 p-[48px]">
-          <div tw="flex h-10" >
-          <img
-            tw="h-full"
-            src={`${websiteUrl}/images/dynamic-og/og-logo-small.svg`}
-            alt="Prism"
-          />
-          </div>
-          <h1 tw="text-[52px]">Join global DeSci Events</h1>
-          <p tw="text-[26px]">Event 1</p>
-        </div>
-        <div tw="flex w-1/2 h-full">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            tw="w-full h-full"
-            src={`${websiteUrl}/images/dynamic-og/og-logo.png`}
-            alt="Prism"
-          />
-        </div>
-      </div>
+// async function handler() {
+//   const fontData = await font;
+
+//   const { upcomingEventsAsc, pastEventsDesc, featuredEvents } = await getAllEvents();
+//   console.log(upcomingEventsAsc)
+
+//   return new ImageResponse(
+//     (
+//       <div tw="flex h-full ">
+//         <div tw="flex flex-col w-1/2 p-[48px]">
+//           <div tw="flex h-10" >
+//           <img
+//             tw="h-full"
+//             src={`${websiteUrl}/images/dynamic-og/og-logo-small.svg`}
+//             alt="Prism"
+//           />
+//           </div>
+//           <h1 tw="text-[52px]">Join global DeSci Events</h1>
+//           <p tw="text-[26px]">Event 1</p>
+//         </div>
+//         <div tw="flex w-1/2 h-full">
+//           {/* eslint-disable-next-line @next/next/no-img-element */}
+//           <img
+//             tw="w-full h-full"
+//             src={`${websiteUrl}/images/dynamic-og/og-logo.png`}
+//             alt="Prism"
+//           />
+//         </div>
+//       </div>
 
    
-  ),
-  {
-    fonts: [
-      {
-        name: "Inter",
-        data: fontData,
-        weight: 500,
-      }
-    ],
-  });
-};
+//   ),
+//   {
+//     fonts: [
+//       {
+//         name: "Inter",
+//         data: fontData,
+//         weight: 500,
+//       }
+//     ],
+//   });
+// };
 
-export default handler;
+// export default handler;
